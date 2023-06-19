4a768c71651e960ec47340f0f960d78bc4d374b1
HAUDEBOURG Baptiste <o22202367@info23-25.iut45.univ-orleans.fr>
2023-06-19 10:22:25 +0200

    Création Enchere

--- Enchere.java
+++ Enchere.java
@@ -1,3 +1,16 @@
+import java.sql.Date;
+
 public class Enchere {
-    
+    private double montant;
+    private Date dateEnchere;
+    public Enchere(double montant, Date date){
+        this.montant =montant;
+        this.dateEnchere =dateEnchere;
+    }
+    public double getMontant(){
+        return this.montant;
+    }
+    public Date getDate(){
+        return this.dateEnchere;
+    }
 }
